142521286dc5ea78c3d24162e6ea778666d57e31
James Rubino <james dot rubino  gmail.com>
2014-07-04 00:29:59 -0500

    added go fmt magic

--- set.go
+++ set.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-       "github.com/conformal/btcjson"
-       "fmt"
-
-       )
+	"fmt"
+	"github.com/conformal/btcjson"
+)
 
 // converts struct to a hopefully unique comma deliminated string
 func Serialize(item btcjson.ListTransactionsResult) string {
 	amt := fmt.Sprintf("%.8f", item.Amount)
 	fee := fmt.Sprintf("%.8f", item.Fee)
-	return item.Address+","+amt+","+fee
+	return item.Address + "," + amt + "," + fee
 }
 
 // converts 100 transactions struct array to string slice
@@ -22,67 +21,64 @@ func SerializeTransactions(data []btcjson.ListTransactionsResult) []string {
 	}
 	return result
 }
-	
 
 // basic Set data structure
 // works with strings for reasons of comparison operations
 type Set struct {
-  data []string
+	data []string
 }
 
 // add element to set
 func (this *Set) Add(element string) {
-  this.data = append(this.data, element)
+	this.data = append(this.data, element)
 }
 
 // remove element from set
-func (this *Set) Remove(element string) (bool) {
-  for _, elem:= range this.data {
-    if elem == element {
-      return true
-    }
-  }
-  return false
+func (this *Set) Remove(element string) bool {
+	for _, elem := range this.data {
+		if elem == element {
+			return true
+		}
+	}
+	return false
 }
 
 // test if element is member of the set
-func (this *Set) IsMember(element string) (bool) {
-  for _, elem:= range this.data {
-    if elem== element {
-      return true
-    }
-  }
-  return false
+func (this *Set) IsMember(element string) bool {
+	for _, elem := range this.data {
+		if elem == element {
+			return true
+		}
+	}
+	return false
 }
 
 // report length of the set
-func (this *Set) Length() (int) {
-  return len(this.data)
+func (this *Set) Length() int {
+	return len(this.data)
 }
 
 // remove duplicates in the set
 func (this *Set) Deduplicate() {
-  length := len(this.data) - 1
-  for i := 0; i < length; i++ {
-    for j := i + 1; j <= length; j++ {
-      if (this.data[i] == this.data[j]) {
-        this.data[j] = this.data[length]
-        this.data = this.data[0:length]
-        length--
-        j--
-      }
-    }
-  }
+	length := len(this.data) - 1
+	for i := 0; i < length; i++ {
+		for j := i + 1; j <= length; j++ {
+			if this.data[i] == this.data[j] {
+				this.data[j] = this.data[length]
+				this.data = this.data[0:length]
+				length--
+				j--
+			}
+		}
+	}
 }
-
 
 // create new set of initial size zero and capacity 100
 // adjust for proper network sampling conditions
 func MakeNewSet() Set {
-  return Set{make([]string, 0, 100)}
+	return Set{make([]string, 0, 100)}
 }
 
-func main(){
+func main() {
 	fmt.Println("Testing 123")
 }
-	
